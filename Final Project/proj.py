17bed72d6ec65cda6c3cec97a22746c7725954c8
chengche6230 <chengche6230@gmail.com>
2022-01-12 23:03:00 +0800

    Code part done.

--- Final Project/proj.py
+++ Final Project/proj.py
@@ -6,9 +6,10 @@ COM530500 Network Science Final Project
 import numpy as np
 import scipy.io
 import networkx as nx
+import matplotlib.pyplot as plt
 
 
-def edge_removal(A,phi):
+def edge_removal(A, phi):
     '''Remove edges by the trick developed by Mollison and Grassberger
     Args:
         A: numpy.ndarray
@@ -25,12 +26,34 @@ def edge_removal(A,phi):
     A_new = ((A * Prob)> 1-phi).astype(int)
     return A_new
 
-mat = scipy.io.loadmat('facebook-ego.mat')
-A = mat['A']  #Adjacency matrix A
+def statisticalInfo(G):
+    print(nx.info(G))
+    print(f'Average clustering coefficient: {nx.algorithms.average_clustering(G)}')
+    print(f'Diameter: {nx.algorithms.diameter(G)}')
+    deg = G.degree()
+    avg_degree = 0
+    max_degree, min_degree = -1, 1e6
+    for i in range(len(deg)):
+        avg_degree += deg[i]
+        max_degree = max(max_degree, deg[i])
+        min_degree = min(min_degree, deg[i])
+    print(f'Average degree: {avg_degree/len(deg)}')
+    print(f'  Maximum degree: {max_degree}')
+    print(f'  Minimum degree: {min_degree}')
+    print(f'Density: {nx.classes.density(G)}')
 
-G = nx.from_numpy_matrix(np.matrix(A)) #Read graph
+if __name__ == "__main__":
+    mat = scipy.io.loadmat('./facebook-ego.mat')
+    A = mat['A']  #Adjacency matrix A
+    A = np.matrix(A)
+    G = nx.from_numpy_matrix(A) #Read graph
+
+    # Problem 1.
+    # statisticalInfo(G)
+    # nx.draw(G)
+    # plt.savefig("visualize.png")
 
 
-
-
-
+    # Problem 2.
+    for i in range(100):
+        pass
